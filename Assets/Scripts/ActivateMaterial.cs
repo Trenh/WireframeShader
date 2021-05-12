1e4ee9eb909e8f0a05953d26de3d63f461e25478
Garcia Gabriel <39272152+Trenh@users.noreply.github.com>
2021-05-12 17:43:35 +0200

    Initial commit

---
+++ Assets/Scripts/ActivateMaterial.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+ 
+public class ActivateMaterial : MonoBehaviour
+{
+    public Material Material;
+    Material[] oldMaterials;
+    bool oldMat;
+    // Use this for initialization
+    void Start()
+    {
+        oldMat = true;
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        oldMaterials = meshRenderer.materials;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("ActiveMat"))
+        {
+            ChangeMaterials();
+        }    
+    }
+
+    void ChangeMaterials()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        if (!oldMat)
+        {
+            meshRenderer.materials = oldMaterials;
+            oldMat = true;
+            return;
+        }
+        
+        //    Material = Resources.Load<Material>("Flat Wireframe");
+        // Get the current material applied on the GameObject
+       Material[] newMaterials = meshRenderer.materials;
+        for (int i = 0; i < oldMaterials.Length; i++)
+        {
+            // Set the new material on the GameObject
+           newMaterials[i] = Material;
+        }
+        meshRenderer.materials = newMaterials;
+        oldMat = false;
+    }
+}
